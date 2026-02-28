f0adf0b9eb91da4b7fb610fa985d06bc7f3eb878
buketyilmaaz <yilmaazbuket@gmail.com>
2026-02-28 16:46:21 +0300

    Add program to calculate average of three numbers

---
+++ uc sayının ortalamsı.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication1.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <stdio.h>
+
+
+	int average(int a, int b, int c) {
+		return (a + b + c) / 3;
+	}int main() {
+		int a, b, c;
+		printf("Please enter the tree numbers whose average you want to calculate:");
+		scanf_s("%d %d %d", &a, &b, &c);
+		int sonuc = average(a, b, c);
+		printf("The average of the numbers you entered =%d\n", sonuc);
+		return 0;
+	}
+	 
