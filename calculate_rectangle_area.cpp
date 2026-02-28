da55c5ead98a5c6e19d905b59513d5eb81ddd0b7
buketyilmaaz <yilmaazbuket@gmail.com>
2026-02-28 16:56:36 +0300

    Add files via upload

---
+++ calculate_rectangle_area.cpp
@@ -0,0 +1,15 @@
+﻿// en son.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include<stdio.h>
+int main()
+{
+	int kenar1, kenar2;
+	int area;
+	printf("dikdorgenlerin kisa ve uzun kenar uzunluklarini giriniz=");
+	scanf_s("%d%d", &kenar1, &kenar2);
+	area = kenar1 * kenar2;
+	printf(" Uzunluklari verilen diktortgenin alanı=%d\n", area);
+	return 0;
+
+	}
