0bc5ee2d443aa9cb9369b4098728e63dc1b9dae0
buketyilmaaz <yilmaazbuket@gmail.com>
2026-02-28 17:04:37 +0300

    find_perfect_number.c

---
+++ MÜKEMMEL SAYI OLUP OLMADIĞINI BELİRLEME.cpp
@@ -0,0 +1,30 @@
+﻿// MÜKEMMEL SAYI OLUP OLMADIĞINI BELİRLEME.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+#include<stdio.h>
+#include <iostream>
+int MükemmelSayiMi(int sayi);
+int main()
+{
+	int sayi;
+	printf(" Enter the number:");
+	scanf_s("%d", &sayi);
+	if (MükemmelSayiMi(sayi))
+	{
+		printf("%d number is a perfect number.", sayi);
+	}
+	else
+	{
+		printf(" %d number is not a perfect number .");
+	} return 0;
+}int MükemmelSayiMi(int sayi)
+{
+	int sum = 0;
+	int i,n;
+	n = sayi;
+	for (i = 1;i < n;i++) {
+		if (n % i == 0) {
+			sum = sum + i;
+		}
+	}
+	return(sum == sayi);
+}
