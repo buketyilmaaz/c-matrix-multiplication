644449b9f91458e9096a6f772a3e3923e79de5b6
buketyilmaaz <yilmaazbuket@gmail.com>
2026-02-28 17:25:56 +0300

    Add files via upload

---
+++ REFERANSA GÖRE ÇAĞIRMA KULLANARK BİR DEĞERİN KÜPÜNÜ ALMAK.cpp
@@ -0,0 +1,23 @@
+﻿// REFERANSA GÖRE ÇAĞIRMA KULLANARK BİR DEĞERİN KÜPÜNÜ ALMAK.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//+
+
+
+
+
+/*KÜÇÜK HARFLERİ BÜYÜK HARFE ÇEVİRME*/
+#include <stdio.h>
+void buyukHarfeCevir(char*);
+int main() {
+	char string[] = "Hello world 123";
+	printf("cevirilmeden önceki string= %s", string);
+	buyukHarfeCevir(string);
+	printf(" \nCevirildikten sonraki string=%s", string);
+	return 0;
+}void buyukHarfeCevir(char*Ptr) {
+	while (*Ptr != '\0') {
+		if (*Ptr >= 'a' && *Ptr <= 'z') {
+			*Ptr = *Ptr - 32;
+		}*Ptr++;
+	}
+}
+
